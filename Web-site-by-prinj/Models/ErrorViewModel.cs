fa2d8c88c012b3641bcb54887807ca29ed4036de
Mr_GreZevS <danil_grezev@mail.ru>
2019-04-15 18:20:40 +0700

    Добавьте файлы проекта.

---
+++ Web-site-by-prinj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_site_by_prinj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
